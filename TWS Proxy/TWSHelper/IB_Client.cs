95ab2456506799927f2ce85460bf19b0dcadf326
Xu Zhe <xuzhe35@hotmail.com>
2016-10-08 23:06:58 +0800

    IB Client Class

---
+++ TWS Proxy/TWSHelper/IB_Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TWSHelper
+{
+    public class IB_Client
+    {
+    }
+}
